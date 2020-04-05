b3a764cb27cc3a05dc73e8ddf9a35e17fa601d20
Sufyan M. Shaikh <sufyanshk@gmail.com>
2020-04-05 22:11:12 +0530

    initial commit

---
+++ kpoint_optmsn.sh
@@ -0,0 +1,46 @@
+#Copyright (C) 2020 Sufyan M. Shaikh
+#!/bin/bash
+
+#THIS FILE SHOULD ONLY BE USED FOR OPTIMISING KPOINTS...
+#AFTER THE E vs V HAS BEEN OPTIMISED AND...
+#THE PRECISE LATTICE PARAMETER HAS BEEN FOUND OUT.
+
+rm summary3.csv summary_with_kp.csv
+
+#Name of the system
+sys_name="V"
+
+#Define no. of cores
+n_cores=8
+
+#Precise lattice parameter, found out from earlier runs of VASP.
+i=2.9
+
+#POSCAR with precise lattice parameter will be written here.
+sed -i "1s/.*/$sys_name/" POSCAR
+sed -i "2s/.*/$i/" POSCAR
+
+#KPOINTS will be changed and the respective energies will be calculated.
+#Put the range of K-point in which you want to get the energy values.
+for kp in $(seq 4 1 20)
+do
+	sed -i "4s/.*/$kp $kp $kp/" KPOINTS
+
+	echo "a= $i K-points=$kp $kp $kp"
+	
+	#Energy will be calculated for every KPOINT
+	mpirun \-n $n_cores vasp_std > log 
+	
+	#Lattice parameter, K-points and energy will be written in the "summary3.csv" file
+	E=`awk '/F=/ {print $0}' OSZICAR` 
+	echo $i $kp $E >> summary3.csv
+done
+
+#This will seperate the columns with commas and the final file will "summary_with_kp.csv".
+awk '{print $1","$2","$5}' summary3.csv >> summary_with_kp.csv
+
+#Plots E vs. K-points for given precise lattice parameter
+gnuplot plot_kp.plt
+
+echo "$sys_name : K-point optimisation finished"
+printf "\n"
